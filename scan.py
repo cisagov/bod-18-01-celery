10d7d1ea0f0e32c50f791ff692ff246327252a09
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-11-09 15:25:21 -0500

    Both trustymail and sslyze scanning are working (in a basic sense)

--- scan.py
+++ scan.py
@@ -21,9 +21,12 @@ import bod1801.tasks
 def main():
     args = docopt.docopt(__doc__, version=bod1801.__version__)
 
-    domain_name = 'dhs.gov'
+    domain_names = [
+        'dhs.gov',
+        'nasa.gov'
+    ]
     smtp_ports = [
-        25
+        25,
     ]
     scan_types = {
         'mx': True,
@@ -35,36 +38,45 @@ def main():
         '8.8.8.8',
         '8.8.4.4'
     ]
-    trustymail_result = bod1801.tasks.trustymail.delay(domain_name=domain_name,
-                                                       smtp_ports=smtp_ports,
-                                                       scan_types=scan_types,
-                                                       dns_hostnames=dns_hostnames)
-    trustymail_answer = trustymail_result.get()
-    print('Trustymail result is: {}'.format(trustymail_answer))
 
-    hostname = trustymail_answer['Mail Servers']
-    port = int(trustymail_answer['Mail Server Ports Tested'])
-    starttls_smtp = True
-    scan_tlsv10 = True
-    scan_tlsv11 = False
-    scan_tlsv12 = False
-    scan_tlsv13 = False
-    scan_sslv20 = False
-    scan_sslv30 = False
-    scan_cert_info = False
+    for domain_name in domain_names:
+        trustymail_result = bod1801.tasks.trustymail.delay(domain_name=domain_name,
+                                                           smtp_ports=smtp_ports,
+                                                           scan_types=scan_types,
+                                                           dns_hostnames=dns_hostnames)
+        trustymail_answer = trustymail_result.get()
+        print('Trustymail result for {} is: {}'.format(domain_name,
+                                                       trustymail_answer))
 
-    sslyze_result = bod1801.tasks.sslyze.delay(hostname=hostname,
-                                               port=port,
-                                               starttls_smtp=starttls_smtp,
-                                               scan_tlsv10=scan_tlsv10,
-                                               scan_tlsv11=scan_tlsv11,
-                                               scan_tlsv12=scan_tlsv12,
-                                               scan_tlsv13=scan_tlsv13,
-                                               scan_sslv20=scan_sslv20,
-                                               scan_sslv30=scan_sslv30,
-                                               scan_cert_info=scan_cert_info)
-    sslyze_answer = sslyze_result.get()
-    print('Sslyze result is: {}'.format(sslyze_answer))
+        servers_and_ports = [s.strip() for s in trustymail_answer['Domain Supports STARTTLS Results'].split(',')]
+
+        for server_and_port in servers_and_ports:
+            temp = server_and_port.split(':')
+            server = temp[0]
+            port = temp[1]
+
+            starttls_smtp = True
+            scan_tlsv10 = True
+            scan_tlsv11 = False
+            scan_tlsv12 = False
+            scan_tlsv13 = False
+            scan_sslv20 = False
+            scan_sslv30 = False
+            scan_cert_info = False
+
+            sslyze_result = bod1801.tasks.sslyze.delay(hostname=server,
+                                                       port=port,
+                                                       starttls_smtp=starttls_smtp,
+                                                       scan_tlsv10=scan_tlsv10,
+                                                       scan_tlsv11=scan_tlsv11,
+                                                       scan_tlsv12=scan_tlsv12,
+                                                       scan_tlsv13=scan_tlsv13,
+                                                       scan_sslv20=scan_sslv20,
+                                                       scan_sslv30=scan_sslv30,
+                                                       scan_cert_info=scan_cert_info)
+            sslyze_answer = sslyze_result.get()
+            print('\tSslyze result for {} is: {}'.format(server_and_port,
+                                                         sslyze_answer))
 
 
 if __name__ == '__main__':
